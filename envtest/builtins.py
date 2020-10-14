8ec65a1f92540af116e666b8dbf7471c9300765a
JihaoXin <46529913+JihaoXin@users.noreply.github.com>
2020-10-14 18:16:08 +0800

    add function about pandas

--- envtest/builtins.py
+++ envtest/builtins.py
@@ -1,8 +1,9 @@
 import numpy as np
 from scipy.ndimage import gaussian_filter
 from scipy import misc
+import pandas as pd
 
-__all__ = ['rand_array', 'smooth_image', 'my_mat_solve']
+__all__ = ['rand_array', 'smooth_image', 'my_mat_solve','df']
 
 
 def rand_array(shape):
@@ -13,3 +14,12 @@ def smooth_image(a, sigma=1):
 
 def my_mat_solve(A, b):
     return A.inv()*b
+
+def pt(x,y):
+	plt.plot(x,y)
+	plt.show()
+	
+
+def df():
+	df = pd.DataFrame(np.random.randn(6, 4),columns=list('ABCD'))
+	print(df)
---
+++ scripts/print_pandas.py
@@ -0,0 +1,3 @@
+from envtest import df
+
+df()
